9fc9009eed4f41d915ca121efd3f0851ddc1b214
Mathieu Dutour <mathieu@dutour.me>
2020-03-06 19:43:29 +0100

    add json schema

--- src/convert/json/logic.ts
+++ src/convert/json/logic.ts
@@ -1,10 +1,18 @@
 import stringify from 'json-stable-stringify'
+import Ajv from 'ajv'
 import * as AST from '../../types/logic-ast'
+import schema from './logic-schema.json'
 
 export function print(ast: AST.SyntaxNode) {
   return stringify(ast, { space: '  ' })
 }
 
 export function parse(contents: string): AST.SyntaxNode {
-  return JSON.parse(contents)
+  const ajv = new Ajv()
+  const validate = ajv.compile(schema)
+  const data = JSON.parse(contents)
+  if (validate(data) && ajv.errors) {
+    throw new Error(ajv.errors[0].message)
+  }
+  return data
 }
