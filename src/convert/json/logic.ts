1b31041a5f7d0a93e12cb266b5121862c1996d1a
Mathieu Dutour <mathieu@dutour.me>
2020-03-11 09:52:53 +0100

    no schema validation

--- src/convert/json/logic.ts
+++ src/convert/json/logic.ts
@@ -1,18 +1,10 @@
 import stringify from 'json-stable-stringify'
-import Ajv from 'ajv'
 import * as AST from '../../types/logic-ast'
-import schema from './logic-schema.json'
 
 export function print(ast: AST.SyntaxNode) {
   return stringify(ast, { space: '  ' })
 }
 
 export function parse(contents: string): AST.SyntaxNode {
-  const ajv = new Ajv()
-  const validate = ajv.compile(schema)
-  const data = JSON.parse(contents)
-  if (validate(data) && ajv.errors) {
-    throw new Error(ajv.errors[0].message)
-  }
-  return data
+  return JSON.parse(contents)
 }
