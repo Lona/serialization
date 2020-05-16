25937a0700b622bf097eea106e8861a11bbc5503
Devin Abbott <devinabbott@gmail.com>
2020-05-15 17:17:25 -0700

    Improve condition type

--- src/types/logic-ast/auxiliary/condition.ts
+++ src/types/logic-ast/auxiliary/condition.ts
@@ -1,20 +1,16 @@
 import { Expression, EnumerationCasePattern, UUID } from '..'
 
 export type ExpressionCondition = {
-  type: 'expressionCondition'
-  data: {
-    id: UUID
-    expression: Expression
-  }
+  id: UUID
+  expression: Expression
 }
 
 export type CaseCondition = {
-  type: 'caseCondition'
-  data: {
-    id: UUID
-    pattern: EnumerationCasePattern
-    initializer: Expression
-  }
+  id: UUID
+  pattern: EnumerationCasePattern
+  initializer: Expression
 }
 
-export type Condition = ExpressionCondition | CaseCondition
+export type Condition =
+  | { type: 'expressionCondition'; data: ExpressionCondition }
+  | { type: 'caseCondition'; data: CaseCondition }
