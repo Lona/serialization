e88337e40b71f241205e4772a1fd8413154bfa6b
Devin Abbott <devinabbott@gmail.com>
2020-05-15 17:05:44 -0700

    Add tests for condition

--- src/convert/swift/pegjs/logicSwiftParser.ts
+++ src/convert/swift/pegjs/logicSwiftParser.ts
@@ -14,6 +14,7 @@ const parser = pegjs.generate(grammar, {
     'declaration',
     'expression',
     'pattern',
+    'condition',
   ],
 })
 
---
+++ src/types/logic-ast/auxiliary/test/condition.test.ts
@@ -0,0 +1,38 @@
+import { parse, print } from '../../../../convert/swift/logic'
+
+jest.mock('uuid', () => ({ v4: () => '0' }))
+
+describe('condition', () => {
+  const parserOptions = { startRule: 'condition' }
+
+  it('expression condition', () => {
+    const code = `a`
+    const ast = parse(code, parserOptions)
+    expect(ast).toMatchSnapshot()
+    expect(print(ast)).toBe(code)
+  })
+
+  it('case condition', () => {
+    const code = `case Test.value(x) = y`
+    const ast = parse(code, parserOptions)
+    expect(ast).toMatchSnapshot()
+    expect(print(ast)).toBe(code)
+  })
+
+  describe('invalid case condition', () => {
+    it('missing parentheses', () => {
+      const code = `case Test.value = y`
+      expect(() => parse(code, parserOptions)).toThrow()
+    })
+
+    it('missing type identifier', () => {
+      const code = `case .value = y`
+      expect(() => parse(code, parserOptions)).toThrow()
+    })
+
+    it('missing initializer identifier', () => {
+      const code = `case .value = y`
+      expect(() => parse(code, parserOptions)).toThrow()
+    })
+  })
+})
